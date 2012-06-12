bffbad854e330a128c34d563f42b1b6b695f9e84
daisieh <credential.helper>
2012-06-12 09:56:53 -0700

    2012.06.11: first pass on parsing CDS tags in genbank files; circlegraph now draws multiple graphs.

--- subfuncs.pl
+++ subfuncs.pl
@@ -3,32 +3,16 @@ use Bio::AlignIO;
 use Bio::SeqIO;
 
 use constant PI 	=> 3.1415926535897932384626433832795;
-use constant MAX_LABEL_LENGTH => 16; # if the gb file contains labels longer than this labelsOK will return FALSE
-use constant MIN_INNER => 1.4; # minimal angle required for non-overlapping inner labels
-use constant MIN_OUTER => 1.1; # minimal angle required for non-overlapping outer labels
-use constant MIN_OPT_OUTER => 1.2; # minimal angle required for outwards-shifted labels
-use constant MIN_OPT_INNER => 1.8; # minimal angle required for inwards-shifted labels
-use constant MAX_OPT_RUNS => 3; # emergency brake for optimization loop
-use constant LABEL_SHIFT => 3.9; # shift for centering of labels above features
-use constant MIN_FRAMED_FEATURE_WIDTH => 0.3; # minimal width for a feature to be drawn with frame
 use constant CENTER_X => 600; # X coordinate of circle center500
 use constant CENTER_Y => 600; # Y coordinate of circle center
 use constant PS_X_SIZE => 1200; # X size of the PostScript object1000
 use constant PS_Y_SIZE => 1200; # Y size of the PostScript object
-use constant CHAR_WIDTH			=> 10; # median char width - - NOT CORRECT
-use constant CHAR_HEIGHT		=> 10; # Character height - - has to be checked again
 
 #
 # graphics default values
 #
 my $MAIN_RADIUS				= 387.5;
-my $RE_LABEL_RADIUS 		= $MAIN_RADIUS + 127.5; # Radius for RE labels
-my $OPT_OUTER_RADIUS 		= $MAIN_RADIUS + 67.5; # radius for outwards-shifted labels
-my $OPT_INNER_RADIUS 		= $MAIN_RADIUS - 67.5; # radius for inwards-shifted labels
-my $INSIDE_FEATURE_RADIUS 	= $MAIN_RADIUS - 12.15;
-my $OUTSIDE_FEATURE_RADIUS 	= $MAIN_RADIUS + 12.15;
-my $INSIDE_LABEL_RADIUS		= $MAIN_RADIUS - 27.5;
-my $OUTSIDE_LABEL_RADIUS	= $MAIN_RADIUS + 27.5;
+my @colours = ([255,0,0],[0,255,0],[0,0,255],[200,200,0],[255,0,255],[0,255,255],[255,100,0],[255,0,100],[100,255,0],[100,0,255]);
 
 sub convert_aln_to_nexus {
 	my $aln = shift;
@@ -50,36 +34,6 @@ sub convert_aln_to_nexus {
  	$result .= "Matrix\n$nexblock\n;\nEnd;\n";
 
 	return $result;
-}
-
-sub return_partition_2 {
-	my $inseq = shift;
-	my $start_pos = shift;
-	my $stop_pos = shift;
-	my $newaln = shift;
-
-	my $seq;
-	eval {$seq = $inseq->next_seq;} or die "3 not fasta\n";
-	my $nexblock = "";
-	my $result = "";
-	my $ntax = 0;
-	my $nchar = 0;
-	while ($seq ne "") {
-		my $len = $seq->length;
-		if ($stop_pos > $len) {
-			return 0;
-		}
- 		my $seq_seg = $seq->subseq($start_pos, $stop_pos);
- 		my $outseq = Bio::LocatableSeq->new(-seq => $seq_seg, -id => $seq->display_name);
- 		$newaln->add_seq ($outseq);
-
- 		$seq = $inseq->next_seq;
-	}
-
-	my $p = $newaln->percentage_identity();
-	print "$start_pos\t$stop_pos\t$p\n";
-
-	return 1;
 }
 
 sub perc_diff_partition {
@@ -157,6 +111,7 @@ sub coords_on_circle {
 
 sub draw_circle_graph_from_file {
 	my $datafile = shift;
+	my $p = shift;
 	my $OUTER_RADIUS = 387.5;
 	my $INNER_RADIUS = 337.5;
 
@@ -165,51 +120,88 @@ sub draw_circle_graph_from_file {
 	my @positions, @differences;
 	my $total_elems;
 	my $max_diffs = 0;
-	while ($line ne "") {
-		$line =~ /(.+?)\t(.+?)$/;
-		$total_elems = push (@positions, $1);
-		my $curr_diffs = $2;
-		push (@differences, $curr_diffs);
-		if ($max_diffs < $curr_diffs) {
-			$max_diffs = $curr_diffs;
+	my @labels = split /\t/, $line;
+	my $num_graphs = @labels-1;
+	print "$num_graphs\n";
+
+	$p->setfont("Helvetica", 12);
+	if (@labels[1] =~ m/.*[:alpha:].*/) {
+		for (my $i = 1; $i <= $num_graphs; $i++) {
+			print "@labels[$i]\n";
+			$p->setcolour($colours[$i-1][0],$colours[$i-1][1],$colours[$i-1][2]);
+			$p->text(10,(30+(12*$i)), "@labels[$i]");
 		}
 		$line = readline $F;
 	}
 
+	while ($line ne "") {
+		my @items = split /\t/, $line;
+		#$line =~ /(.+?)\t(.+?)$/;
+		my $pos = shift @items;
+		$total_elems = push (@positions, $pos);
+		push (@differences, @items);
+		$line = readline $F;
+	}
+
+	my @sorted = sort (@differences);
+	my $diff_len = @sorted;
+	$max_diffs = @sorted[@sorted-1];
 	my $window_size = @positions[1]-@positions[0];
 	my $circle_size = @positions[$total_elems-1];
 
-	my $p = new PostScript::Simple( colour => 1, eps => 0, units => "bp", xsize => PS_X_SIZE, ysize => PS_Y_SIZE );
-	$p->newpage;
-
+	$p->setcolour(black);
+	$p->setlinewidth(1);
 	$p->circle(CENTER_X,CENTER_Y, $OUTER_RADIUS);
 	$p->circle(CENTER_X,CENTER_Y, $INNER_RADIUS);
+	$p->setlinewidth(2);
 
-	my @coords = coords_on_circle(0,$INNER_RADIUS);
-	my ($last_x, $last_y, $this_x, $this_y);
-	$last_x = @coords[0];
-	$last_y = @coords[1];
+	for (my $j = 0; $j < $num_graphs; $j++) {
+		my @coords = coords_on_circle(0,$INNER_RADIUS);
+		my ($last_x, $last_y, $this_x, $this_y);
+		$last_x = @coords[0];
+		$last_y = @coords[1];
 
-	$p->setcolour(red);
+		$p->setcolour($colours[$j][0],$colours[$j][1],$colours[$j][2]);
+		for (my $i = 0; $i < $total_elems; $i++) {
+			my $angle = (@positions[$i]/$circle_size) * 360;
+			#my $radius = $INNER_RADIUS + (($OUTER_RADIUS-$INNER_RADIUS)*(@differences[$i]/$max_diffs));
+			my $radius = $INNER_RADIUS + (($OUTER_RADIUS-$INNER_RADIUS)*(@differences[($i*$num_graphs)+$j]/$max_diffs));
+			my @new_coords = coords_on_circle($angle,$radius);
+			$this_x = @new_coords[0];
+			$this_y = @new_coords[1];
+			$p->line($last_x, $last_y, $this_x, $this_y);
+			$last_x = $this_x;
+			$last_y = $this_y;
+		}
+		$p->line($last_x, $last_y, @coords[0], @coords[1]);
+	}
+
+	$p->setfont("Helvetica", 6);
+	$p->setcolour(black);
+
 	for (my $i = 0; $i < $total_elems; $i++) {
 		my $angle = (@positions[$i]/$circle_size) * 360;
-		my $radius = $INNER_RADIUS + (($OUTER_RADIUS-$INNER_RADIUS)*(@differences[$i]/$max_diffs));
+		my $radius = $OUTER_RADIUS + 10;
 		my @new_coords = coords_on_circle($angle,$radius);
-		$this_x = @new_coords[0];
-		$this_y = @new_coords[1];
-		$p->line($last_x, $last_y, $this_x, $this_y);
-		$last_x = $this_x;
-		$last_y = $this_y;
+		$p->text( {rotate => $angle}, @new_coords[0], @new_coords[1], "@positions[$i]");
 	}
-	$p->line($last_x, $last_y, @coords[0], @coords[1]);
+
 	$p->setfont("Helvetica", 12);
 	$p->setcolour(black);
 	$p->text(10, 10, "Maximum percent difference ($max_diffs) is scaled to 1");
 	$p->text(10, 30, "Sliding window size of $window_size bp");
 
-	# write the output to a file
-	$p->output("file.ps");
 }
 
-
+sub main_name_for_gb_feature {
+	my $feat = shift;
+	my @names;
+	my $curr_name = "n/a";
+	eval {@names = $feat->get_tag_values('locus_tag');} or @names="";
+	if (@names) {
+		$curr_name = @names[0];
+	}
+	eval {@names = $feat->get_tag_values('gene');} or return "$curr_name";
+	return @names[0];
+}
 1;
--- test_biopl.pl
+++ test_biopl.pl
@@ -1,12 +1,31 @@
 require "subfuncs.pl";
+use Bio::SeqIO;
 use constant CENTER_X => 600; # X coordinate of circle center500
 use constant CENTER_Y => 600; # Y coordinate of circle center
 use constant PS_X_SIZE => 1200; # X size of the PostScript object1000
 use constant PS_Y_SIZE => 1200; # Y size of the PostScript object
 
-# Bring in the file and format, or die with a nice
-# usage statement if one or both arguments are missing.
 my $usage  = "test_biopl.pl \n";
-my $transcripts = shift or die $usage;
 
-draw_circle_graph_from_file ($transcripts);
+my $gb_file = shift or die $usage;
+
+my $seqio_object = Bio::SeqIO->new(-file => $gb_file);
+my $seq_object = $seqio_object->next_seq;
+
+while ($seq_object) {
+	for my $feat_object ($seq_object->get_SeqFeatures) {
+		if ($feat_object->primary_tag eq "CDS") {
+			my $name = main_name_for_gb_feature($feat_object);
+			my @locations = $feat_object->location->each_Location;
+			my $loc_str = "";
+			foreach $loc (@locations) {
+				my $start = $loc->start;
+				my $end = $loc->end;
+				$loc_str .= "($start..$end)";
+			}
+			print "$name\t$loc_str\n";
+		}
+	}
+	$seq_object = $seqio_object->next_seq;
+}
+
