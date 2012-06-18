d7d327b08f644cc6dcac5f563a43c6a7c3fb1bf5
daisieh_local <daisieh@gmail.com>
2012-06-18 14:02:54 -0700

    now concats all exons and uses the correct strand.

--- slice_fasta_from_genbank.pl
+++ slice_fasta_from_genbank.pl
@@ -1,4 +1,6 @@
 use Bio::SeqIO;
+use Bio::Align::Utilities qw(cat);
+
 require "subfuncs.pl";
 
 # Bring in the file and format, or die with a nice
@@ -8,31 +10,55 @@ my $gb_file   = shift or die $usage;
 my $fa_file = shift or die $usage;
 my $result_dir = shift or die $usage;
 
-my $start_pos = 10;
-my $stop_pos = 50;
-my $inseq;
-$inseq = Bio::SeqIO->new(-file => "<$gb_file", -format => "genbank") or die "not genbank\n";
-eval {$seq = $inseq->next_seq;} or die "1 not fasta\n";
-my @seq_features = $seq->get_SeqFeatures;
-foreach my $feature (@seq_features) {
-	if ($feature->has_tag("protein_id")) {
-		my @feat_tags;
-		my $flag = 1;
-		eval {@feat_tags = $feature->get_tag_values("gene")} or {$flag = 0};
-		if ($flag) {
-			my $gene_name = @feat_tags[0];
-			$start_pos = $feature->start;
-			$stop_pos = $feature->end;
-			print "writing $gene_name, $start_pos-$stop_pos...\n";
-			my $outfile = "$result_dir\/$gene_name.nex";
-			my $curr_aln = make_aln_from_fasta_file($fa_file);
-			my $sub_aln = $curr_aln->slice($start_pos, $stop_pos);
-			my $result = convert_aln_to_nexus ($sub_aln);
-			open my $gene_file, ">$outfile";
-			truncate $gene_file, 0;
-			print $gene_file $result;
-			close $gene_file;
+my $whole_aln = make_aln_from_fasta_file ($fa_file);
+my @gene_alns;
+
+my $seqio_object = Bio::SeqIO->new(-file => $gb_file);
+my $seq_object = $seqio_object->next_seq;
+
+while ($seq_object) {
+	for my $feat_object ($seq_object->get_SeqFeatures) {
+		if ($feat_object->primary_tag eq "CDS") {
+			my $name = main_name_for_gb_feature($feat_object);
+			my @locations = $feat_object->location->each_Location;
+			my $cat_aln = 0;
+			my $strand = 0;
+			foreach $loc (@locations) {
+				$strand = $loc->strand;
+				my $start = $loc->start;
+				my $end = $loc->end;
+				my $curr_slice = $whole_aln->slice($start, $end);
+				if ($cat_aln == 0) {
+					$cat_aln = $curr_slice;
+				} else {
+					$cat_aln = cat($cat_aln, $curr_slice);
+				}
+			}
+			if ($strand < 0) {
+				# must flip each seq in the curr_slice
+				my $flipped_aln = Bio::SimpleAlign->new();
+				foreach $seq ( $cat_aln->each_seq() ) {
+					$seq = $seq->revcom();
+					$flipped_aln->add_seq($seq);
+				}
+				$cat_aln = $flipped_aln;
+			}
+
+			$cat_aln = $cat_aln->slice(1, $cat_aln->length()-3);
+			$cat_aln->description($name);
+			push @gene_alns, $cat_aln;
 		}
 	}
+	$seq_object = $seqio_object->next_seq;
 }
 
+foreach my $aln (@gene_alns) {
+	my $gene_name = $aln->description();
+	print "writing $gene_name...\n";
+	my $outfile = "$result_dir\/$gene_name.nex";
+	my $result = convert_aln_to_nexus ($aln);
+	open my $gene_file, ">$outfile";
+	truncate $gene_file, 0;
+	print $gene_file $result;
+	close $gene_file;
+}
