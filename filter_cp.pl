597d94819e477e88ee1755ec50c21f9621578802
daisieh_local <daisieh@gmail.com>
2012-10-04 17:08:47 -0700

    adding new functionality to filter_cp.pl

--- filter_cp.pl
+++ filter_cp.pl
@@ -1,19 +1,33 @@
 #!/usr/bin/perl
 use strict;
 use File::Basename;
-
+use Getopt::Long;
+use Pod::Usage;
 
 my $usage = "perl " . basename($0);
 $usage .=	" <fastafile> <cp.fasta> <resultfile>\n\n";
 
-my $fastafile = shift or die "$usage";
-my $cpfile = shift or die "$usage";
-my $resultfile = shift or die "$usage";
-my $filterfile = "$fastafile.filtered";
+my ($fastafile, $reffile, $resultfile, $task) = 0;
+my $evalue = 10;
+GetOptions ('fasta=s' => \$fastafile,
+            'reference=s' => \$reffile,
+            'task=s' => \$task,
+            'evalue:i' => \$evalue) or pod2usage(-msg => "GetOptions failed.", -exitval => 2);
+
+unless ($gb_file && $fa_file && $tree_file && $output_name) {
+    my $msg = qq{Error: an option was mis-specified:
+    fasta=$fastafile
+    reference=$reffile
+    task=$task
+    };
+    pod2usage(-msg => $msg, -exitval => 2);
+}
+
+my $filterfile = "$resultfile.filtered";
 
 print "blasting...";
 
-my @blastargs = ("blastn", "-evalue", "10", "-subject", "$cpfile", "-query", "$fastafile", "-outfmt", "6 qseqid bitscore evalue", "-out", "$filterfile");
+my @blastargs = ("blastn", "-evalue", "$evalue", "-subject", "$reffile", "-query", "$fastafile", "-outfmt", "6 qseqid bitscore evalue", "-out", "$filterfile");
 system (@blastargs);
 
 print "done (results in $filterfile)\n";
@@ -25,7 +39,7 @@ open my $this_file, "<$filterfile" or die "error opening filtered seqs file";
 
 my $filtering = readline $this_file;
 if (eof($this_file)) {
-	print "no sequences blasted to e. coli\n";
+	print "no sequences blasted to reference.\n";
 	$filtering = "%%%";
 }
 
@@ -59,23 +73,27 @@ while ($fs ne "") {
 			last;
 		}
 	}
-	if ($filtering ne "%%%") {
-# 		print "checking $filtering...";
-		$filtering =~ /(.+?)\t/;
-		my $curr_seq = $1;
+    if ($task eq "discard-hits") {
+        last;
+    } else {
+        if ($filtering ne "%%%") {
+            $filtering =~ /(.+?)\t/;
+            my $curr_seq = $1;
 
-		if ($first_header =~ m/$curr_seq/) {
-			$filtering = readline $this_file;
-			print $outfile "$first_header$sequence";
-			while ($filtering =~ m/$curr_seq/) {
-				$filtering = readline $this_file;
-				if ($filtering eq "") {
-					$filtering = "%%%";
-					last;
-				}
-			}
-		}
-	}
+            if ($first_header =~ m/$curr_seq/) {
+                $filtering = readline $this_file;
+                print $outfile "$first_header$sequence";
+                while ($filtering =~ m/$curr_seq/) {
+                    $filtering = readline $this_file;
+                    if ($filtering eq "") {
+                        $filtering = "%%%";
+                        last;
+                    }
+                }
+            }
+        }
+
+    }
 }
 
 print "done\n";
@@ -83,3 +101,24 @@ print "done\n";
 close $this_file;
 close $F;
 close $outfile;
+
+__END__
+
+=head1 NAME
+
+tree_omega
+
+=head1 SYNOPSIS
+
+filter_cp [options]
+
+=head1 OPTIONS
+
+    -fasta:     fasta file to filter
+    -reference: fasta file of the reference to be filtered against
+    -task:      "discard-hits" means to discard hits to the reference
+                "keep-hits" means to keep hits
+
+=head1 DESCRIPTION
+
+=cut
