aec3520d42ff20b96beef37fcfb71550ba0dbe05
daisieh_local <daisieh@gmail.com>
2012-10-10 12:09:43 -0700

    optimized code

--- fasta_lengths.pl
+++ fasta_lengths.pl
@@ -3,13 +3,18 @@ my $file = shift;
 open FH, "<", $file;
 
 my $line = readline FH;
+my ($name, $length) = 0;
 while ($line) {
-	$line =~ />(.*)\s+/;
-	my $name = $1;
+    if ($line =~ />(.*)\s+/) {
+        if ($length > 0) {
+            print "$name\t$length\n";
+        }
+        $name = $1;
+        $length = 0;
+    } else {
+        $length += length ($line) - 1;
+	}
 	$line = readline FH;
-	my $length = length ($line) - 1;
-	$line = readline FH;
-	print "$name\t$length\n";
 }
-
+print "$name\t$length\n";
 close FH;
