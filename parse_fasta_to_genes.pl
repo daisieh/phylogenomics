ad13d1921546e0f336bd8c26418f31d1f7dc4e99
daisieh_local <daisieh@gmail.com>
2012-10-26 15:20:45 -0700

    Added POD documentation.

--- parse_fasta_to_genes.pl
+++ parse_fasta_to_genes.pl
@@ -1,23 +1,28 @@
 use Bio::SeqIO;
 use Bio::Align::Utilities qw(cat);
-
+use Pod::Usage;
+use File::Basename;
 require "subfuncs.pl";
 
-# Bring in the file and format, or die with a nice
-# usage statement if one or both arguments are missing.
-my $usage  = "parse_fasta_to_genes.pl genbank_file fasta_file out_file\n";
-my $gb_file   = shift or die $usage;
-my $fa_file = shift or die $usage;
-my $out_file = shift or die $usage;
+if (@ARGV == 0) {
+    pod2usage(-verbose => 1);
+}
+
+my ($fa_file, $gb_file, $out_file, $help) = 0;
+GetOptions ('fasta=s' => \$fa_file,
+            'genbank|gb_file=s' => \$gb_file,
+            'outputfile=s' => \$out_file,
+            'help|?' => \$help) or pod2usage(-msg => "GetOptions failed.", -exitval => 2);
+
+if ($help) {
+    pod2usage(-verbose => 1);
+}
 
 my $whole_aln = make_aln_from_fasta_file ($fa_file);
+my $gb_seqio = Bio::SeqIO->new(-file => $gb_file);
 my @gene_alns;
 
-my $seqio_object = Bio::SeqIO->new(-file => $gb_file);
-my $seq_object = $seqio_object->next_seq;
-
-my $result_str = "";
-while ($seq_object) {
+while (my $seq_object = $gb_seqio->next_seq) {
 	for my $feat_object ($seq_object->get_SeqFeatures) {
 		if ($feat_object->primary_tag eq "gene") {
 			my $name = main_name_for_gb_feature($feat_object);
@@ -34,7 +39,6 @@ while ($seq_object) {
 				} else {
 					$cat_aln = cat($cat_aln, $curr_slice);
 				}
-				if ($result_str eq "") { $result_str = "$name\t$start"; }
 			}
 			if ($strand < 0) {
 				# must flip each seq in the curr_slice
@@ -49,24 +53,43 @@ while ($seq_object) {
 			$cat_aln = $cat_aln->slice(1, $cat_aln->length()-3);
 			$cat_aln->description($name);
 			push @gene_alns, $cat_aln;
-			$result_str = "";
 		}
 	}
-	$seq_object = $seqio_object->next_seq;
 }
 
-	#print "writing $gene_name...\n";
-# my $seq_out = Bio::SeqIO->new(-file => ">$out_file", -format => "fasta");
-	open my $gene_file, ">$out_file.fasta";
+open my $gene_file, ">$out_file.fasta";
 
 foreach my $aln (@gene_alns) {
 	my $gene_name = $aln->description();
 	foreach my $seq ($aln->each_seq()) {
-		my $name = $gene_name . "_" . $seq->length();
+		my $name = $seq->id() . "_$gene_name: " . $seq->length();
 		print $gene_file ">$name\n";
 		print $gene_file $seq->seq() . "\n";
-# 		my $tempseq = Bio::Seq::RichSeq->new(-seq => $seq->seq(), -id => $name, -accession_number => "");
-# 		$seq_out->write_seq($tempseq);
 	}
 }
-	close $gene_file;
+close $gene_file;
+
+__END__
+
+=head1 NAME
+
+parse_fasta_to_genes
+
+=head1 SYNOPSIS
+
+parse_fasta_to_genes [-fasta fa_file] [-genbank gb_file] [-outputfile output_file]
+
+=head1 OPTIONS
+
+  -fasta:           fasta file of aligned sequences
+  -genbank|gb_file: genbank file with CDS coordinates
+  -outputfile:      prefix of output files
+
+=head1 DESCRIPTION
+
+Given a fasta file of aligned sequences and a corresponding genbank file
+with the CDS coordinates, will create a fasta file with each
+CDS corresponding to a separate sequence block.
+
+=cut
+
