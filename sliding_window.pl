7747986dd822be396646e7bcb6c72c82e9195f91
daisieh_local <daisieh@gmail.com>
2012-10-26 14:37:01 -0700

    Cleaned up code, added documentation.

--- sliding_window.pl
+++ sliding_window.pl
@@ -1,4 +1,3 @@
-# use PostScript::Simple;
 use CircleGraph;
 use File::Basename;
 use Getopt::Long;
@@ -6,20 +5,26 @@ use Pod::Usage;
 require "subfuncs.pl";
 require "circlegraphs.pl";
 
-print "running " . basename($0) . " " . join (" ", @ARGV) . "\n";
 
-my ($fastafile, $datafile, $out_file, $gb_file, $window_size) = 0;
+my ($fastafile, $datafile, $out_file, $gb_file, $window_size, $help) = 0;
 GetOptions ('fasta:s' => \$fastafile,
             'data:s' => \$datafile,
-            'outputfile:s' => \$out_file,
+            'outputfile=s' => \$out_file,
             'genbank|gb_file:s' => \$gb_file,
-            'window:i' => \$window_size) or pod2usage(-msg => "GetOptions failed.", -exitval => 2);
+            'window:i' => \$window_size,
+            'help|?' => \$help) or pod2usage(-msg => "GetOptions failed.", -exitval => 2);
+
+if ((@ARGV == 0) || ($help)) {
+    pod2usage(-verbose => 1);
+}
+
+print "running " . basename($0) . " " . join (" ", @ARGV) . "\n";
 
 my $circle_size;
 
 if ($gb_file =~ /\.gb$/) {
     open FH, ">", "$out_file.genes";
-    print FH parse_genbank_file($gb_file);
+    print FH get_locations_from_genbank_file($gb_file);
     close FH;
 }
 
@@ -53,14 +58,14 @@ if ($fastafile) {   # if we were given a fasta file, we should create the diffs 
 }
 
 # using the data file to make a graph
-my $x = draw_circle_graph($datafile);
+my $circlegraph_obj = draw_circle_graph($datafile);
 
 if ($window_size > 0) {
-    $x->append_to_legend("Sliding window of $window_size bp");
+    $circlegraph_obj->append_to_legend("Sliding window of $window_size bp");
 }
 
-$x->set_font("Helvetica", 12, "black");
-$x->draw_legend_text;
+$circlegraph_obj->set_font("Helvetica", 12, "black");
+$circlegraph_obj->draw_legend_text;
 
 
 if ($gb_file) {
@@ -89,28 +94,28 @@ if ($gb_file) {
 
         my $start_angle = ($start/$circle_size) * 360;
         my $stop_angle = ($stop/$circle_size) * 360;
-        my $radius = $x->inner_radius;
+        my $radius = $circlegraph_obj->inner_radius;
 
-        $x->set_percent_red((1-$value)*100);
-        $x->draw_filled_arc ($radius, $start_angle, $stop_angle);
+        $circlegraph_obj->set_percent_red((1-$value)*100);
+        $circlegraph_obj->draw_filled_arc ($radius, $start_angle, $stop_angle);
 
         # label this element
         my $center_angle = ($start_angle + $stop_angle) / 2;
         push @labels, "$label\t$center_angle";
     }
 
-    $x->draw_circle($x->inner_radius - 5, {filled => 1, color => "white"});
-    $x->draw_circle($x->inner_radius);
-    $x->set_font("Helvetica", 6, "black");
+    $circlegraph_obj->draw_circle($circlegraph_obj->inner_radius - 5, {filled => 1, color => "white"});
+    $circlegraph_obj->draw_circle($circlegraph_obj->inner_radius);
+    $circlegraph_obj->set_font("Helvetica", 6, "black");
     foreach my $line (@labels) {
         $line =~ /(.+?)\t(.+?)$/;
-        $x->circle_label($2, $x->inner_radius - 5, $1, "right");
+        $circlegraph_obj->circle_label($2, $circlegraph_obj->inner_radius - 5, $1, "right");
     }
 }
 
-$x->output_ps();
+$circlegraph_obj->output_ps();
 open OUT, ">", "$out_file.ps" or die "couldn't make output file $out_file";
-print OUT $x->output_ps . "\n";
+print OUT $circlegraph_obj->output_ps . "\n";
 close OUT;
 
 __END__
@@ -121,14 +126,23 @@ sliding_window
 
 =head1 SYNOPSIS
 
-sliding_window [options]
+sliding_window [-fasta -window | -data] [-genbank] [-output]
 
 =head1 OPTIONS
 
-    -fasta:     fasta file of aligned sequences
-	-outputfile:    prefix of output files
-	-genbank|gb_file:	genbank file specifying genes
+  -fasta:           fasta file of aligned sequences
+  -window:          if -fasta is specified, size of sliding window
+  -data:            previously generated differences file
+  -genbank|gb_file: (optional) genbank file to make gene map
+  -outputfile:      prefix of output files
 
 =head1 DESCRIPTION
+
+Given a fasta file and a sliding window size, calculates
+the percent difference for each sliding window and maps it to a circular graph.
+
+Can graph a previously-generated differences file as well.
+Optionally, if a corresponding genbank file is specified,
+will draw a gene map along the inside of the circle.
 
 =cut
