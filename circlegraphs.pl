cf3662bd37fe2294037b5954db0f851f78effd34
daisieh_local <daisieh@gmail.com>
2012-10-26 14:35:26 -0700

    Clarified code, added documentation.

--- circlegraphs.pl
+++ circlegraphs.pl
@@ -2,9 +2,25 @@ use CircleGraph;
 use Bio::AlignIO;
 use Bio::SeqIO;
 
+=pod
+
+CircleGraph $circlegraph_obj draw_circle_graph ( String $datafile, CircleGraph $circlegraph_obj )
+
+$datafile is a tab-delimited file:
+    -   the first column contains positions around a circle,
+        scaled to the size of the last row's value
+    -   each subsequent column contains the values to graph, one graph per column
+    -   the first row contains the names of the columns
+$circlegraph_obj is an optional parameter for an existing CircleGraph.
+
+The function returns a CircleGraph object with the values plotted,
+positions mapped around the edge, and a legend describing the graph.
+
+=cut
+
 sub draw_circle_graph {
     my $datafile = shift;
-    my $x = shift;
+    my $circlegraph_obj = shift;
 
     open DATAFH, "<$datafile" or die "$datafile failed to open\n";
     my $max_diffs = 0;
@@ -46,32 +62,32 @@ sub draw_circle_graph {
         }
     }
 
-    unless ($x) {
-        $x = new CircleGraph();
+    unless ($circlegraph_obj) {
+        $circlegraph_obj = new CircleGraph();
     }
 
     # draw background circles
-    $x->draw_circle($x->inner_radius);
-    $x->draw_circle($x->outer_radius);
+    $circlegraph_obj->draw_circle($circlegraph_obj->inner_radius);
+    $circlegraph_obj->draw_circle($circlegraph_obj->outer_radius);
 
     for (my $j=0; $j<@graphs; $j++) {
-        $x->set_color($j);
-        $x->plot_line(\@positions, $graphs[$j]);
-        $x->append_to_legend("@labels[$j]", "$j");
+        $circlegraph_obj->set_color($j);
+        $circlegraph_obj->plot_line(\@positions, $graphs[$j]);
+        $circlegraph_obj->append_to_legend("@labels[$j]", "$j");
     }
 
     # draw labels around the edge
-    $x->set_font("Helvetica", 6, "black");
+    $circlegraph_obj->set_font("Helvetica", 6, "black");
     my $circle_size = @positions[@positions-1];
 
     for (my $i = 0; $i < $total_elems; $i++) {
         my $angle = (@positions[$i]/$circle_size) * 360;
-        my $radius = $x->outer_radius + 10;
-        $x->circle_label($angle, $radius, "@positions[$i]");
+        my $radius = $circlegraph_obj->outer_radius + 10;
+        $circlegraph_obj->circle_label($angle, $radius, "@positions[$i]");
     }
 
-    $x->append_to_legend("Maximum percent difference ($max_diffs) is scaled to 1");
-    return $x;
+    $circlegraph_obj->append_to_legend("Maximum percent difference ($max_diffs) is scaled to 1");
+    return $circlegraph_obj;
 }
 
 # must return 1 for the file overall.
