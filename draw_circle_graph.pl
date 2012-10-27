8dd3c195102847ca26e73d4787dec6003651237a
daisieh_local <daisieh@gmail.com>
2012-10-26 18:31:40 -0700

    updated POD text.

--- draw_circle_graph.pl
+++ draw_circle_graph.pl
@@ -5,36 +5,71 @@ use Pod::Usage;
 require "subfuncs.pl";
 require "circlegraphs.pl";
 
-my ($datafile, $outfile) = 0;
-GetOptions ('data=s' => \$datafile,
-            'outputfile=s' => \$outfile) or pod2usage(-msg => "GetOptions failed.", -exitval => 2);
+if (@ARGV == 0) {
+    pod2usage(-verbose => 1);
+}
 
-if (($datafile==0) && ($outfile==0)) {
+my $runline = "running " . basename($0) . " " . join (" ", @ARGV) . "\n";
+
+my ($datafile, $outfile, $gb_file, $help) = 0;
+
+GetOptions ('datafile|inputfile=s' => \$datafile,
+            'outputfile=s' => \$outfile,
+            'genbank|gb_file:s' => \$gb_file,
+            'help|?' => \$help) or pod2usage(-msg => "GetOptions failed.", -exitval => 2);
+
+if ($help) {
+    pod2usage(-verbose => 1);
+}
+print $runline;
+
+
+unless (($datafile) && ($outfile)) {
+print "$datafile, $outfile\n";
     pod2usage(-msg => "GetOptions failed.", -exitval => 2);
 }
 
-my $x = draw_circle_graph($datafile);
-$x->set_font("Helvetica", 12, "black");
-$x->draw_legend_text;
+my $circlegraph_obj = draw_circle_graph($datafile);
+$circlegraph_obj->set_font("Helvetica", 12, "black");
+$circlegraph_obj->draw_legend_text;
+
+if ($gb_file) {
+	if ($gb_file =~ /\.gb$/) {
+		open FH, ">", "$outfile.genes";
+		print FH get_locations_from_genbank_file($gb_file);
+		close FH;
+	    $gb_file = "$outfile.genes";
+	}
+	draw_gene_map ($gb_file, $circlegraph_obj);
+}
 
 open OUT, ">", "$outfile.ps" or die "couldn't make output file $outfile.ps";
-print OUT $x->output_ps . "\n";
+print OUT $circlegraph_obj->output_ps . "\n";
 close OUT;
 
 __END__
 
 =head1 NAME
 
-tree_omega
+draw_circle_graph
 
 =head1 SYNOPSIS
 
-draw_circle_graph [options]
+draw_circle_graph [-data] [-genbank] [-output]
 
 =head1 OPTIONS
-    -datafile:      name of tab-delimited file with positions as first column, graphs as subsequent columns.
-	-outputfile:    name of output file.
+
+  -data:            table to graph, with positions in the first column and values to
+                    graph in subsequent columns.
+  -genbank|gb_file: (optional) genbank file to make gene map
+  -outputfile:      prefix of output files
 
 =head1 DESCRIPTION
+
+Graphs values, given a file with a table of values plotted
+along positions on a circle.
+
+Optionally, if a corresponding genbank file is specified,
+will draw a gene map along the inside of the circle.
 
 =cut
