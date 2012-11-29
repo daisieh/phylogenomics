f607687e32bae953bd8933c59739cec55416b4f2
daisieh_local <daisieh@gmail.com>
2012-11-29 15:30:46 -0800

    first commit

---
+++ converting/rename_fasta.pl
@@ -0,0 +1,24 @@
+require "subfuncs.pl";
+
+my $fastafile = shift;
+my $labelfile = shift;
+
+my $fa_aln = make_aln_from_fasta_file ($fastafile, 0);
+
+print STDERR "boo " .  $fa_aln->length() . "\n";
+
+my $labels;
+$labels = make_label_lookup ($labelfile);
+
+foreach my $seqio ($fa_aln->each_seq) {
+	my $key = $seqio->id();
+	my $label = $key;
+	if (exists $labels->{$key}) {
+		$label = $labels->{$key};
+	}
+	print ">$label\n";
+	print $seqio->seq() . "\n";
+}
+
+
+
