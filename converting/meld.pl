2621781140a618eadaca137ceb90da7ac91bb848
daisieh_local <daisieh@gmail.com>
2013-04-05 12:23:59 -0700

    Meld.pl is more robust: takes either nexus or fasta and returns either.

--- converting/meld.pl
+++ converting/meld.pl
@@ -1,6 +1,7 @@
 #!/usr/bin/perl
 use strict;
-sub uninterleave;
+sub parse_nexus;
+sub parse_fasta;
 my @inputfiles;
 push @inputfiles, @ARGV;
 
@@ -10,19 +11,45 @@ my %regiontable = ();
 $regiontable{ "regions" } = "";
 $regiontable{ "exclusion-sets" } = "";
 my $currlength = 0;
-my $outfolder = "./";
+my $outname = "melded";
+my $outformat = "nexus";
 my @matrixnames = ();
 
-#foreach my $inputfile (@inputfiles) {
+if ((@ARGV == 0) || (@ARGV[1] =~ /-h/) || (@ARGV[1] =~ /-u/)) {
+	print "meld.pl [-output outfile] [-format nex|fasta] inputfile1 inputfile2...\n";
+	print qq{
+Melds aligned sequence files of either fasta or nexus format.
+Sequences with the same names will be concatenated; sequences missing
+from any particular alignment will be concatenated as missing data.
+A tab-delimited table of the aligned regions and the sequences present
+in each will be output to outfile_regions.tab.
+
+The program defaults to output into melded.nex and melded_regions.tab.
+};
+	exit;
+}
+
+
 for (my $i=0; $i< scalar(@inputfiles); $i++) {
 	my $inputfile = @inputfiles[$i];
 	if ($inputfile eq "-output") {
-		$outfolder = @inputfiles[$i+1];
+		$outname = @inputfiles[$i+1];
+		$i++;
+		next;
+	} elsif ($inputfile eq "-format") {
+		$outformat = @inputfiles[$i+1];
 		$i++;
 		next;
 	}
+
 	push @matrixnames, $inputfile;
-	$matrices{ $inputfile } = uninterleave $inputfile;
+	if ($inputfile =~ /\.nex/) {
+		$matrices{ $inputfile } = parse_nexus $inputfile;
+	} elsif ($inputfile =~/\.fa/) {
+		$matrices{ $inputfile } = parse_fasta $inputfile;
+	} else {
+		print "Couldn't parse $inputfile: not nexus or fasta format\n";
+	}
 	while ( my ($key, $value) = each(%{$matrices{ $inputfile }}) ) {
         $mastertaxa{ $key } = "";
     }
@@ -74,7 +101,7 @@ delete $regiontable{"length"};
 my $ntax = keys %mastertaxa;
 my $nchar = $l;
 
-open (fileOUT, ">$outfolder". "regions.tab") or die "couldn't make $outfolder/regions.tab\n";
+open (fileOUT, ">$outname"."_regions.tab") or die "couldn't make $outname"."_regions.tab\n";
 truncate fileOUT, 0;
 print fileOUT "regions\t$regiontable{'regions'}\n";
 print fileOUT "exclusion_sets\t$regiontable{'exclusion-sets'}\n";
@@ -85,22 +112,32 @@ foreach my $key (keys %regiontable) {
 }
 close fileOUT;
 
-#print melded matrix to melded.nex
-open (fileOUT, ">$outfolder/melded.nex") or die "couldn't make melded.nex\n";
-truncate fileOUT, 0;
-print fileOUT "#NEXUS\nBegin DATA;\n";
-print fileOUT "Dimensions ntax=$ntax nchar=$nchar;\n";
-print fileOUT "Format datatype=NUCLEOTIDE gap=-;\nMatrix\n";
-foreach my $key ( keys %mastertaxa ) {
-	my $value = $mastertaxa{$key};
-	print fileOUT "$key\t$value\n";
+#print melded matrix to output file
+if ($outformat =~ /fa/) {
+	open (fileOUT, ">$outname".".".$outformat) or die "couldn't make $outname"."."."$outformat\n";
+	truncate fileOUT, 0;
+	foreach my $key ( keys %mastertaxa ) {
+		my $value = $mastertaxa{$key};
+		print fileOUT ">$key\n$value\n";
+	}
+	close fileOUT;
+} else {
+	open (fileOUT, ">$outname".".nex") or die "couldn't make $outname".".nex\n";
+	truncate fileOUT, 0;
+	print fileOUT "#NEXUS\nBegin DATA;\n";
+	print fileOUT "Dimensions ntax=$ntax nchar=$nchar;\n";
+	print fileOUT "Format datatype=NUCLEOTIDE gap=-;\nMatrix\n";
+	foreach my $key ( keys %mastertaxa ) {
+		my $value = $mastertaxa{$key};
+		print fileOUT "$key\t$value\n";
+	}
+	print fileOUT ";\nEnd;\n";
+	close fileOUT;
 }
-print fileOUT ";\nEnd;\n";
-close fileOUT;
 
-print "Melded matrix is in melded.nex; summary of regions is in regions.tab\n";
+print "Melded matrix is in $outname".".$outformat; summary of regions is in $outname"."_regions.tab\n";
 
-sub uninterleave {
+sub parse_fasta {
 	my $inputfile = shift(@_);
 
 	my %taxa = ();
@@ -127,6 +164,64 @@ sub uninterleave {
 	}
 
 	close (fileIN);
+	return \%taxa;
+}
+
+sub parse_nexus {
+	my $inputfile = shift(@_);
+	open (fileIN, "$inputfile") or die "no file named $inputfile";
+	my @inputs = <fileIN>;
+
+	my $input = "";
+	if ($inputs[1] eq "") {
+		$input = $inputs[0];
+		$input =~ s/\r/\n/gs;
+	} else {
+		foreach my $line (@inputs) {
+			$input .= "$line";
+		}
+	}
+	close (fileIN);
+
+	#remove comment blocks
+	$input =~ s/\[.*?\]//sg;
+
+	$input =~ /Format(.*?)\;/ig;
+	my $format = "$1";
+	$format =~ /gap=(.)/;
+	my $gapchar = $1;
+
+	#parse nexus block
+	$input =~ /Matrix(.*?)\;/isg;
+	my $matrix = "$1";
+
+	$matrix =~ /\s*?(\S+?)\s+/s;
+	my $firsttaxon = "$1";
+	my %taxa = ();
+
+	my @sections = split /$firsttaxon/, $matrix;
+	my @taxonlabels;
+	foreach my $section (@sections) {
+		$section =~ s/\s+$//s;
+		if ($section eq "") { next; }
+		$section = "$firsttaxon$section";
+		$section =~ s/\s+$/\n/s;
+		$section =~ s/\t//sg;
+		my $numtaxa = ($section =~ s/\n/\n/sg);
+
+		@taxonlabels = split /\n/, $section;
+
+		foreach my $taxonlabel (@taxonlabels) {
+			$taxonlabel =~ s/\s+(.+?)$//;
+			my $taxondata = $1;
+			$taxondata =~ s/$gapchar/-/g;
+			$taxa{ $taxonlabel } = $taxa{ $taxonlabel } . $taxondata;
+		}
+	}
+
+	my $length = length $taxa{ $taxonlabels[0] };
+	$taxa{ "length" } = $length;
+
 	return \%taxa;
 }
 
