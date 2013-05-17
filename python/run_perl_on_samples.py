3a01cfc7ca6f794bcda6181cceb6535ba43ecbc4
daisieh_local <daisieh@gmail.com>
2013-05-17 12:55:15 -0700

    run a given script on a pool of samples

---
+++ python/run_perl_on_samples.py
@@ -0,0 +1,56 @@
+import sys, os
+import optparse
+from socket import gethostname
+from multiprocessing import Pool
+
+scriptname = "";
+
+def assign_script( script ):
+	global scriptname
+	scriptname = script;
+
+def stop_err( msg ):
+    sys.stderr.write( "%s\n" % msg )
+    sys.exit()
+
+def runscript(sample_string):
+    cmd = "perl %s -input %s.vcf" % (scriptname,sample_string)
+    print(cmd)
+    os.system(cmd)
+
+def __main__():
+    #Parse Command Line
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--input", default=None, dest="input",
+                      help="The input location file")
+    parser.add_option("-s", "--script", default=None, dest="script",
+                      help="The script to batch")
+    (options, args) = parser.parse_args()
+
+    try:
+        open(options.input, "r").close()
+    except TypeError, e:
+        stop_err("You need to supply the input file:\n" + str(e))
+
+    try:
+        open(options.script, "r").close()
+    except TypeError, e:
+        stop_err("Script file not found:\n" + str(e))
+
+	assignscriptname(options.script)
+
+    pool = Pool(processes=10)
+
+    #read the location file
+    handle = open(options.input, "r")
+    samples = []
+    for line in handle:
+        sample = line.rstrip()
+        samples.append(sample)
+    handle.close()
+
+    #this can be run in parallel
+    pool.map(runscript, samples)
+
+if __name__=="__main__":
+    __main__()
