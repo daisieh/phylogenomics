c6b3f686dff3099a461220ecbf4cd52f1ea725e4
daisieh_local <daisieh@gmail.com>
2013-07-10 17:17:05 -0700

    vcf2fasta now uses phred genotype scores to choose alt allele.

--- converting/vcf2fasta.pl
+++ converting/vcf2fasta.pl
@@ -11,16 +11,14 @@ my $runline = "running " . basename($0) . " " . join (" ", @ARGV) . "\n";
 
 my $samplefile = 0;
 my $help = 0;
-my $cov_thresh = 10;
-my $alt_thresh = 0.8;
-my $keep_ambig = 0;
-my $outfile = "result";
+my $cov_thresh = 5;
+my $pl_thresh = 0;
+my $outfile = "";
 
-GetOptions ('samples|input=s' => \$samplefile,
-            'outputfile:s' => \$outfile,
-            'threshold:i' => \$alt_thresh,
-            'minimum|coverage:i' => \$cov_thresh,
-            'iupac|ambiguity' => \$keep_ambig,
+GetOptions ('samples|input|vcf=s' => \$samplefile,
+            'outputfile=s' => \$outfile,
+            'threshold|pl=i' => \$pl_thresh,
+            'minimum|coverage|reads=i' => \$cov_thresh,
             'help|?' => \$help) or pod2usage(-msg => "GetOptions failed.", -exitval => 2);
 
 if ($help) {
@@ -34,14 +32,13 @@ if ($samplefile =~ /(.*?)\.vcf$/) {
 	@samples = ($1);
 }
 
-if (@samples == 1) {
-	unless ($outfile) {
-		$outfile = $samples[0];
-	}
-}
 my $result_str = "";
+my $name;
+
+my @AMBIGUOUS_POS = (0,"N",".",0,0,0);
 
 foreach my $sample (@samples) {
+	$name = basename($sample);
 	my $vcf_file = $sample . ".vcf";
 	open VCF_FH, "<", $vcf_file or die "couldn't open input file $vcf_file.";
 
@@ -57,21 +54,28 @@ foreach my $sample (@samples) {
 	#Populus_trichocarpa_chloroplast	1	.	A	.	84	.	DP=18;AF1=7.924e-12;CI95=1.5,0;DP4=18,0,0,0;MQ=59	PL	0
 	my $i=1;
 	while ($line !~ m/^\s*$/) {
-		my ($chrom,$pos,undef,$ref,$alt,$qual,$filter,$info,undef) = split (/\t/,$line);
+		my @this_pos = ();
+		my ($chrom,$pos,undef,$ref,$alt,$qual,$filter,$info,undef,$pl) = split (/\t/,$line);
 		if ($pos != $i) { # there is a gap in the vcf file: add Ns until we get to the next listed position.
-			my @this_pos = (0,"N",".",0,0);
+			push @this_pos, @AMBIGUOUS_POS;
 			push @positions, \@this_pos;
 		} else {
-			$info =~ /DP=(.*?);/;
-			my $depth = $1;
-			$info =~ /.*?DP4=(\d+?),(\d+?),(\d+?),(\d+?);/;
-			my $ref_depth = $1 + $2;
-			my $alt_depth = $3 + $4;
-			if ($pos != $last_pos) {
-				my @this_pos = ($depth,$ref,$alt,$ref_depth,$alt_depth);
+			if ($line =~ /INDEL/) { # we don't handle indels; drop these lines.
+				push @this_pos, @AMBIGUOUS_POS;
 				push @positions, \@this_pos;
+			} else {
+				$info =~ /DP=(.*?);/;
+				my $depth = $1;
+				$info =~ /.*?DP4=(\d+?),(\d+?),(\d+?),(\d+?);/;
+				my $ref_depth = $1 + $2;
+				my $alt_depth = $3 + $4;
+				chomp $pl;
+				if ($pos != $last_pos) {
+					push @this_pos, ($depth,$ref,$alt,$ref_depth,$alt_depth,$pl);
+					push @positions, \@this_pos;
+				}
+				$last_pos = $pos;
 			}
-			$last_pos = $pos;
 			$line = readline VCF_FH;
 		}
 		$i++;
@@ -80,37 +84,55 @@ foreach my $sample (@samples) {
 	close VCF_FH;
 
 	my $seq = "";
-	print "processed sample $sample with length " . @positions . "\n";
+	print "processed sample $name with length " . @positions . "\n";
 	for (my $i=0;$i<@positions; $i++) {
 		my $ptr = @positions[$i];
 		my @this_pos = @$ptr;
+		my $pos = $i+1;
 		my $depth = @this_pos[0];
 		my $ref = @this_pos[1];
 		my $alt = @this_pos[2];
 		my $ref_depth = @this_pos[3];
 		my $alt_depth = @this_pos[4];
+		my $pl = @this_pos[5];
+
 		if ($depth > $cov_thresh) {
-			if ($alt_depth/($alt_depth+$ref_depth) > $alt_thresh) {
-				if ($keep_ambig) {
-					$alt = get_iupac_code([$ref, $alt]);
+			if ($pl =~ /,/) {
+				$alt = "N";
+				my @pls = split(/,/, $pl);
+				my $alleles = get_allele_str([$ref, $alt]);
+				my @genotypes = @{get_ordered_genotypes($alleles)};
+				foreach (my $i=0;$i<@pls;$i++) {
+					if ($pls[$i] <= $pl_thresh) {
+						$alt = get_iupac_code($genotypes[$i]);
+					}
 				}
 				$seq .= $alt;
-			} elsif ($ref_depth/($alt_depth+$ref_depth) > $alt_thresh) {
+			} elsif ($pl <= $pl_thresh) {
 				$seq .= $ref;
 			} else {
-				$seq .= "-";
+				$seq .= "N";
 			}
 		} else {
-			$seq .= "-";
+			$seq .= "n";
 		}
 	}
 
-	$result_str .= ">$sample\n$seq\n";
+	$result_str .= ">$name\n$seq\n";
 }
+
+if (@samples == 1) {
+	unless ($outfile ne "") {
+		$outfile = $name;
+	}
+}
+
 
 open FH, ">", $outfile.".fasta";
 print FH "$result_str";
 close FH;
+
+
 
 __END__
 
@@ -126,8 +148,8 @@ vcf2fasta -samplefile -output [-threshold]
 
   -samples|input|vcf:   name of sample or list of samples to convert
   -outputfile:      optional: prefix of output fasta file
-  -threshold:		optional: threshold ratio to accept alternate allele (default is 0.8)
-  -min|coverage:    optional: coverage required to call base (default is 1000)
+  -threshold|pl:	optional: maximum threshold for Phred-scaled likelihood (lower is best, default is 0)
+  -min|coverage:    optional: read depth required to call base (default is 5)
 
 =head1 DESCRIPTION
 
