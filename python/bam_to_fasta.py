33aef76ec497ffb9940e9892be796544b855fc13
daisieh_local <daisieh@gmail.com>
2013-08-21 11:51:26 -0700

    python script to take raw bam from west/east and make fasta file.

---
+++ python/bam_to_fasta.py
@@ -0,0 +1,42 @@
+import sys, os
+import optparse
+from socket import gethostname
+from multiprocessing import Pool
+
+def stop_err( msg ):
+    sys.stderr.write( "%s\n" % msg )
+    sys.exit()
+
+def runscript(sample_string):
+    host,sample,location = sample_string.split()
+    cmd = "samtools view %s | gawk \'{print \">$1\n$10\"}\' - > %s.fasta" % (location, sample)
+    os.system(cmd)
+
+def __main__():
+    #Parse Command Line
+    parser = optparse.OptionParser()
+    parser.add_option("-i", "--input", default=None, dest="input",
+                      help="A list of files to run script on")
+    parser.add_option("-p", "--processes", default=None, dest="processes",
+                      help="Number of processes to use")
+    (options, args) = parser.parse_args()
+
+    try:
+        open(options.input, "r").close()
+    except TypeError, e:
+        stop_err("You need to supply the input file:\n" + str(e))
+
+    pool = Pool(processes=int(options.processes))
+
+    #read the location file
+    handle = open(options.input, "r")
+    samples = []
+    for line in handle:
+        sample = line.rstrip()
+        samples.append(sample)
+    handle.close()
+
+    pool.map(runscript, samples)
+
+if __name__=="__main__":
+    __main__()
