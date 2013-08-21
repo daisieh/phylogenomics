890622afe3a030b08e05479d318cb845caa8891e
daisieh_local <daisieh@gmail.com>
2013-08-21 12:28:43 -0700

    catch paired-ends

--- converting/bam_to_fasta.sh
+++ converting/bam_to_fasta.sh
@@ -1,1 +1,1 @@
-samtools view $1 | awk '{print ">"$1"\n"$10}' - > $2.fasta
+samtools view $1 | awk '{if ($2 == 77) print ">"$1"/1\n"$10; if ($2 == 141) print ">"$1"/2\n"$10; if (($2 != 77) && ($2 != 141)) print ">"$1"\n"$10;}' - > $2.fasta
