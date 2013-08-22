89d603330a6004fa303c1acc5d1a14f7acc05699
daisieh_local <daisieh@gmail.com>
2013-08-22 12:13:36 -0700

    need to break after printing a line.

--- converting/bam_to_fasta.sh
+++ converting/bam_to_fasta.sh
@@ -1,1 +1,1 @@
-samtools view $1 | gawk '{if (and($2,0x0200)) next; if (and($2,0x0040)) print ">"$1"/1\n"$10; if (and($2,0x0080)) print ">"$1"/2\n"$10; else print ">"$1"\n"$10;}' - > $2.fasta
+samtools view $1 | gawk '{if (and($2,0x0200)) next; if (and($2,0x0040)) {print ">"$1"/1\n"$10; next;} if (and($2,0x0080)) {print ">"$1"/2\n"$10; next;} print ">"$1"\n"$10;}' - > $2.fasta
