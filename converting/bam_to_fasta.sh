f813deb6111c4a186f390476791d73bfc17dba37
daisieh_local <daisieh@gmail.com>
2013-08-22 11:57:28 -0700

    does not save the seq if it fails quality checks.

--- converting/bam_to_fasta.sh
+++ converting/bam_to_fasta.sh
@@ -1,1 +1,1 @@
-samtools view $1 | awk '{if ($2 == 77) print ">"$1"/1\n"$10; if ($2 == 141) print ">"$1"/2\n"$10; if (($2 != 77) && ($2 != 141)) print ">"$1"\n"$10;}' - > $2.fasta
+samtools view $1 | gawk '{if (and($2,0x0200)) next; if (and($2,0x0040)) print ">"$1"/1\n"$10; if (and($2,0x0080)) print ">"$1"/2\n"$10; else print ">"$1"\n"$10;}' - > $2.fasta
