1aaf9f48c2794ef6f38e875128661d9d9ed66177
daisieh_local <daisieh@gmail.com>
2013-09-12 14:40:33 -0700

    changed condition for catching blank records

--- filtering/sort_fasta_by_length.sh
+++ filtering/sort_fasta_by_length.sh
@@ -1,4 +1,4 @@
 infile=$1
 outfile=$1.sorted.fasta
-gawk '{if ($0 - /^$/) next; s = ""; for (i=2;i<=NF;i++) s = s$i; print length(s)","$1","s}' RS=">" $infile | sort -n -r | gawk '{sub ("_","",$2); print ">" $2 "\n" $3 "\n"}' FS=","> $outfile
-#| sort -n -r | gawk '{sub ("_","",$2); print ">" $2 "\n" $3 "\n"}' FS=","
+gawk '{if (NF == 0) next; s = ""; for (i=2;i<=NF;i++) s = s$i; print length(s)","$1","s}' RS=">" $infile | sort -n -r | gawk '{sub ("_","",$2); print ">" $2 "\n" $3 "\n"}' FS=","> $outfile
+#| sort -n -r | gawk '{sub ("_","",$2); print ">" $2 "\n" $3}' FS=","
