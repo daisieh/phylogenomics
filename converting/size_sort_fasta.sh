e09e06f803e1d64d8b2c3ba8d7e6bd81dbf4556d
daisieh_local <daisieh@gmail.com>
2013-09-13 18:20:21 -0700

    merge and remove extra files.

--- converting/size_sort_fasta.sh
+++
@@ -1,4 +0,0 @@
-# sorts a fasta file by size of seqs, largest first.
-
-infile=$1
-gawk '{if ($0 - /^$/) next; s = ""; for (i=2;i<=NF;i++) s = s$i; print length(s)","$1","s}' RS=">" $infile | sort -n -r | gawk '{ print ">"$2"\n"$3 }' FS=","
--- filtering/sort_fasta_by_length.sh
+++ filtering/sort_fasta_by_length.sh
@@ -1,4 +1,5 @@
+# sorts a fasta file by size of seqs, largest first.
+
 infile=$1
 outfile=$1.sorted.fasta
-gawk '{if (NF == 0) next; s = ""; for (i=2;i<=NF;i++) s = s$i; print length(s)","$1","s}' RS=">" $infile | sort -n -r | gawk '{sub ("_","",$2); print ">" $2 "\n" $3 "\n"}' FS=","> $outfile
-#| sort -n -r | gawk '{sub ("_","",$2); print ">" $2 "\n" $3}' FS=","
+gawk '{if (NF == 0) next; s = ""; for (i=2;i<=NF;i++) s = s$i; print length(s)","$1","s}' RS=">" $infile | sort -n -r | gawk '{print ">"$2"\n"$3}' FS=","> $outfile
