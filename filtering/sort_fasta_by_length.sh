34d16ab859dce0615a02f747f77a9ad49a2bb00b
daisieh_local <daisieh@gmail.com>
2013-09-17 18:13:55 -0700

    it was never broken.

--- filtering/sort_fasta_by_length.sh
+++ filtering/sort_fasta_by_length.sh
@@ -1,5 +1,4 @@
 # sorts a fasta file by size of seqs, largest first.
 
 infile=$1
-outfile=$1.sorted.fasta
-gawk '{if (NF == 0) next; s = ""; for (i=2;i<=NF;i++) s = s$i; print length(s)","$1","s}' RS=">" $infile | sort -n -r | gawk '{print ">"$2"\n"$3}' FS=","> $outfile
+gawk '{if (NF == 0) next; s = ""; for (i=2;i<=NF;i++) s = s$i; print length(s)","$1","s}' RS=">" $infile | sort -n -r | gawk '{print ">"$2"\n"$3}' FS=","
