10abe38ec8331b9ed66811353e58a8799771a76e
daisieh_local <daisieh@gmail.com>
2013-09-17 18:13:43 -0700

    simple script to make a consensus seq from a fasta alignment.

---
+++ filtering/consensus.pl
@@ -0,0 +1,9 @@
+use strict;
+require "bioperl_subfuncs.pl";
+
+my $input_file = shift;
+
+my $aln = make_aln_from_fasta_file($input_file);
+my $consensus = $aln->consensus_iupac();
+
+print ">$input_file\n$consensus\n";
