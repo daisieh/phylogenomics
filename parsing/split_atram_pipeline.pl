2dac6b338c4c9aca65fbc185a059bdd44ea23a48
daisieh_local <daisieh@gmail.com>
2014-01-28 21:32:13 -0800

    Takes a single fasta file of sequences and makes a sequence list for aTRAM's AlignmentPipeline.pl.

---
+++ parsing/split_atram_pipeline.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl
+use File::Spec;
+require "subfuncs.pl";
+
+my $fastafile = shift;
+my $outdir = shift;
+
+my ($seqs, $seqnames) = parse_fasta($fastafile);
+$outdir = File::Spec->rel2abs($outdir);
+
+open LISTFH, ">", File::Spec->catfile( $outdir, "fastalist.txt" );
+
+foreach my $t (@$seqnames) {
+	my $clean_name = $t;
+	$clean_name =~ s/\|.*$//;
+	my $seqfile = File::Spec->catfile( $outdir, "$clean_name.fasta" );
+	print LISTFH "$clean_name\t$seqfile\n";
+	open FH, ">", $seqfile;
+	print FH ">$t\n$taxa->{$t}\n";
+	close FH;
+}
+
+close LISTFH;
