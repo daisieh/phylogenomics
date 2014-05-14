bd9b00a2819d4f9d8c2974da1d27adfda6622ac3
daisieh_local <daisieh@gmail.com>
2014-05-13 23:14:05 -0700

    reduce the bam file first, then aln.

---
+++ filtering/subset_sra.pl
@@ -0,0 +1,8 @@
+my $bamfile = shift;
+my $frac = shift;
+
+my $totallines = `samtools view -c $bamfile`;
+my $fraclines = $totallines * $frac;
+$fraclines =~ s/\.*//;
+
+print `samtools view $bamfile | head -n $fraclines`;
