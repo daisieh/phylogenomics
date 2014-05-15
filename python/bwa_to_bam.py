6cf8a1cb7e7b8845e091b9660e265590fd79b540
daisieh_local <daisieh@gmail.com>
2014-05-14 18:34:31 -0700

    first rewrite

--- python/bwa_to_bam.py
+++ python/bwa_to_bam.py
@@ -1,81 +1,82 @@
-import sys, os
+import sys
 import optparse
-from socket import gethostname
 from multiprocessing import Pool
+from subprocess import Popen, PIPE, call
 
-# refname = "~/Populus/reference_seqs/populus.trichocarpa.cp.fasta";
+def runcommand(cmd):
+    try:
+        retcode = call(cmd, shell=True)
+        if retcode < 0:
+            print >>sys.stderr, "Child was terminated by signal", -retcode
+        else:
+            print >>sys.stderr, "Child returned", retcode
+    except OSError as e:
+        sys.exit ("Execution of "+cmd+" failed: "+str(e))
 
 
-def stop_err( msg ):
-    sys.stderr.write( "%s\n" % msg )
-    sys.exit()
 
 def runscript(sample_string):
-    """
-    remote filters the bam file
-    """
+    print >>sys.stdout, "sample", sample_string
     host,sample,location = sample_string.split()
 
-    #This happens locally
-    cmd = "samtools faidx %s" % (refname)
-    os.system(cmd)
-    cmd = "samtools view %s | head -n %i | samtools view -S -u -t %s.fai - > %s.small.bam" % (location, lines, refname, sample)
-    sys.stderr.write( "%s\n" % cmd )
-    os.system(cmd)
-    cmd = "bwa aln %s -b1 %s.small.bam > %s.1.sai" % (refname,sample,sample)
-    os.system(cmd)
-    cmd = "bwa aln %s -b2 %s.small.bam > %s.2.sai" % (refname,sample,sample)
-    os.system(cmd)
+    p1 = Popen(["samtools", "view", location], stdout=PIPE)
+    p2 = Popen(["head", "-n", str(lines)], stdin=p1.stdout, stdout=PIPE)
+
+    smallbamfilename = str(sample+".small.bam")
+    smallbamfile = open(smallbamfilename, "w")
+    p3 = Popen(["samtools", "view", "-S", "-u", "-t", refname+".fai", "-"], stdin=p2.stdout, stdout=smallbamfile)
+    smallbamfile.close()
+
+    cmd = "bwa aln -b1 %s %s > %s.1.sai" % (refname,smallbamfilename,sample)
+    print >>sys.stdout, cmd
+    runcommand(cmd)
+    cmd = "bwa aln -b2 %s %s > %s.2.sai" % (refname,smallbamfilename,sample)
+    runcommand(cmd)
     cmd = "bwa sampe %s %s.1.sai %s.2.sai %s.small.bam %s.small.bam > %s.sam" % (refname,sample,sample,sample,sample,sample)
-    os.system(cmd)
+    runcommand(cmd)
     cmd = "rm %s.1.sai; rm %s.2.sai; rm %s.small.bam" % (sample,sample,sample)
-    os.system(cmd)
+    runcommand(cmd)
     cmd = "samtools view -S -b -u -o %s.bam %s.sam" % (sample,sample)
-    os.system(cmd)
+    runcommand(cmd)
     cmd = "rm %s.sam" % (sample)
-    os.system(cmd)
+    runcommand(cmd)
 
-def __main__():
-    global refname
-    global lines
-    #Parse Command Line
-    parser = optparse.OptionParser()
-    parser.add_option("-i", "--input", type="string", default=None, dest="input",
-                      help="A list of files to run script on")
-    parser.add_option("-r", "--reference", type="string", default="~/Populus/reference_seqs/populus.trichocarpa.cp.fasta", dest="ref",
-                      help="The reference genome")
-    parser.add_option("-p", "--processes", default=1, type="int", dest="processes",
-                      help="Number of processes to use")
-    parser.add_option("-n", "--number", default=50000000, type="int", dest="num",
-                      help="Number of short reads to use")
+global refname
+global lines
+#Parse Command Line
+parser = optparse.OptionParser()
+parser.add_option("-i", "--input", type="string", default="", dest="input", help="A list of files to run script on")
+parser.add_option("-r", "--reference", type="string", default="~/Populus/reference_seqs/populus.trichocarpa.cp.fasta", dest="ref", help="The reference genome")
+parser.add_option("-p", "--processes", default=2, type="int", dest="processes", help="Number of processes to use")
+parser.add_option("-n", "--number", default=5000, type="int", dest="num", help="Number of short reads to use")
 
-    (options, args) = parser.parse_args()
-    refname = options.ref
-    lines = options.num
+(options, args) = parser.parse_args()
+refname = options.ref
+lines = options.num
 
-    try:
-        open(options.input, "r").close()
-    except TypeError, e:
-        stop_err("You need to supply the input file:\n" + str(e))
+if options.input == "":
+    sys.exit("Sample file must be provided.\n")
 
-    try:
-        open(options.ref, "r").close()
-    except TypeError, e:
-        stop_err("Reference file not found:\n" + str(e))
+try:
+    open(options.ref, "r").close()
+    cmd = "samtools faidx %s" % (refname)
+    runcommand(cmd)
+except IOError as e:
+    sys.exit("Reference file " + options.ref + " not found\n")
 
-    print ("using ", refname, " with ", lines)
+print >>sys.stdout, "using "+refname+" with "+str(lines)
 
-    pool = Pool(int(options.processes))
+pool = Pool(int(options.processes))
 
-    #read the location file
+#read the location file
+try:
     handle = open(options.input, "r")
     samples = []
     for line in handle:
         sample = line.rstrip()
         samples.append(sample)
     handle.close()
+except IOError as e:
+    sys.exit("Sample file " + options.input + " not found\n")
 
-    pool.map(runscript, samples)
-
-if __name__=="__main__":
-    __main__()
+pool.map(runscript, samples)
