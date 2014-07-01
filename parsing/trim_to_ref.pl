80bdc5d67ecf0fc226ebe0fa5ab1af0d23fa1cea
daisieh_local <daisieh@gmail.com>
2014-06-30 19:21:29 -0700

    rewritten to use aTRAM's functions instead.

--- parsing/trim_to_ref.pl
+++ parsing/trim_to_ref.pl
@@ -1,8 +1,16 @@
+#!/usr/bin/env perl
 use strict;
-use File::Basename;
+use File::Temp qw/ tempfile tempdir /;
 use Getopt::Long;
 use Pod::Usage;
-require "bioperl_subfuncs.pl";
+use File::Basename;
+use FindBin;
+use lib "$FindBin::Bin/..";
+use Subfunctions qw(split_seq reverse_complement meld_matrices parse_fasta);
+use lib "$FindBin::Bin";
+use Blast qw (blast_to_ref);
+use lib "../../../Sandbox/aTRAM/lib";
+use Postprocessing qw(align_to_ref trim_to_ref align_to_seq);
 
 if (@ARGV == 0) {
     pod2usage(-verbose => 1);
@@ -10,88 +18,68 @@ if (@ARGV == 0) {
 
 my $runline = "running " . basename($0) . " " . join (" ", @ARGV) . "\n";
 
-my ($fastafile, $outfile, $reference, $ref_seq) = 0;
-my $window_size = 1000;
+my $ref_file = 0;
+my $align_file = 0;
+my $out_file = 0;
 my $help = 0;
+my $ref_out = 0;
 
-GetOptions ('fasta:s' => \$fastafile,
-            'outputfile=s' => \$outfile,
-            'reference:s' => \$reference,
+GetOptions ('fasta|input=s' => \$align_file,
+            'outputfile=s' => \$out_file,
+            'reference=s' => \$ref_file,
+            'include_ref' => \$ref_out,
             'help|?' => \$help) or pod2usage(-msg => "GetOptions failed.", -exitval => 2);
 
 if ($help) {
     pod2usage(-verbose => 1);
 }
 
+unless($ref_file and $align_file and $out_file) {
+    pod2usage(-msg => "Must specify reference file, alignment file, and output file.");
+}
+
 print $runline;
 
-my $master_alignment = make_aln_from_fasta_file ($fastafile);
+my (undef, $alnfile) = tempfile(UNLINK => 1);
+my $aligned = align_to_ref ($ref_file, $align_file, $alnfile, "mafft");
 
-if ($reference ne "") {
-    $master_alignment = $master_alignment->set_new_reference($reference);
-} else {
-    $reference = $master_alignment->get_seq_by_pos(1)->id();
-}
+trim_to_ref ($aligned, "reference");
 
-# look over the reference sequence: find regions that are not gaps
-my @regions = ();
-$ref_seq = $master_alignment->get_seq_by_pos(1)->seq();
-my $curr_pos = 0;
-my $position;
-my @sequences = ();
-foreach my $seqio ($master_alignment->each_seq) {
-	my $seq = "";
-	push @sequences, \$seq;
+open OUT_FH, ">", $out_file;
+
+my $ref = delete $aligned->{"reference"};
+
+if ($ref_out == 1) {
+	print OUT_FH ">reference\n$ref\n";
 }
 
-while ($ref_seq =~ m/-/gc) {
-	$position = pos($ref_seq);
-	my @reg_pair = ($curr_pos, $position);
-	if (!$position) {
-		@reg_pair[1] = length ($ref_seq);
-		push @regions, \@reg_pair;
-		last;
-	}
-	push @regions, \@reg_pair;
-	$ref_seq =~ m/-*/gc;
-	$curr_pos = pos($ref_seq);
-}
-# then, for all of those regions, push the slices into a new SimpleAlign.
-foreach my $reg_ptr (@regions) {
-	my @reg_pair = @$reg_ptr;
-	$curr_pos = @reg_pair[0];
-	$position = @reg_pair[1];
-	for (my $i=0; $i<@sequences; $i++) {
-		${@sequences[$i]} .= substr($master_alignment->get_seq_by_pos($i+1)->seq(),$curr_pos,($position-$curr_pos-1));
-	}
+foreach my $seq (keys %$aligned) {
+	print OUT_FH ">$seq\n$aligned->{$seq}\n";
 }
 
-open FH, ">", $outfile or die "couldn't make output file $outfile";
-foreach (my $i=0; $i<@sequences; $i++) {
-	my $name = $master_alignment->get_seq_by_pos($i+1)->id();
-	print FH ">$name\n".${@sequences[$i]}."\n";
-}
-close FH;
+close OUT_FH;
 
 __END__
 
 =head1 NAME
 
-trim_to_ref
+blast_to_ref
 
 =head1 SYNOPSIS
 
-trim_to_ref -fasta -output [-reference]
+trim_to_ref -fasta fastafile -reference reffile -output outputfile
 
 =head1 OPTIONS
-  -fasta:           fasta file of aligned sequences
-  -outputfile:      prefix of output files
-  -reference:       optional: name of sequence to be used as reference (default is first seq)
+
+  -fasta|input:     fasta file of aligned sequences.
+  -reference:       fasta file with sequences of interest.
+  -outputfile:      output file name.
+  -include_ref:     optional: if specified, includes the reference sequence in the fasta output.
 
 =head1 DESCRIPTION
 
-Given a fasta file of aligned sequences, removes gaps from the reference sequence and
-trims the rest of the alignment to match.
+Takes a fasta file and finds aligned regions in each sequence in the fasta file that
+match the reference sequence(es). Returns a fasta file of aligned regions of similarity.
 
 =cut
 
