55654d348590ab4c30c18cc928deb44f9df77d45
daisieh_local <daisieh@gmail.com>
2014-07-11 15:37:25 -0700

    use tricho by default, but allow others.

--- pipelines/bam_to_plastome_vcf.sh
+++ pipelines/bam_to_plastome_vcf.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+REF=$2;
+if [ -z $2 ];
+then
+REF="~/Populus/reference_seqs/populus.trichocarpa.cp.fasta";
+fi
+
 for f in $1 #or whatever files contain the formatted input files for bwa_to_bam.py
 do
 	echo $f
 
 	# this cuts out the first ~2.2GB of the unsorted bam file: equivalent to >1000 reads/bp
-	python ~/phylogenomics/python/bwa_to_bam.py -i $f -r ~/Populus/reference_seqs/populus.trichocarpa.cp.fasta -p 8 -n 10000000
-	python ~/phylogenomics/python/bam_to_vcf.py -i $f -r ~/Populus/reference_seqs/populus.trichocarpa.cp.fasta -p 8
+	python ~/phylogenomics/python/bwa_to_bam.py -i $f -r $REF -p 8 -n 10000000
+	python ~/phylogenomics/python/bam_to_vcf.py -i $f -r $REF -p 8
 done
