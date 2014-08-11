acb7f9d42bd18ff80c3079db9653accba7947cab
daisieh_local <daisieh@gmail.com>
2014-08-11 15:32:08 -0700

    ifs

--- pipelines/bam_to_plastome_vcf.sh
+++ pipelines/bam_to_plastome_vcf.sh
@@ -10,9 +10,15 @@ echo "reference is $REF";
 
 for f in $1 #or whatever files contain the formatted input files for bwa_to_bam.py
 do
+	if [! -f $f]
+	then
+	echo "$f doesn't exist"
+	fi
+	if [ -f $f ]
+	then
 	echo "looking in $f"
-
 	# this cuts out the first ~2.2GB of the unsorted bam file: equivalent to >1000 reads/bp
 	python ~/phylogenomics/python/bwa_to_bam.py -i $f -r $REF -p 8 -n 10000000
 	python ~/phylogenomics/python/bam_to_vcf.py -i $f -r $REF -p 8
+	fi
 done
