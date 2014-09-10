a33b6816bd6cb90ec65282048e03367a12327f3a
daisieh_local <daisieh@gmail.com>
2014-09-09 18:19:23 -0700

    more informative comment

--- pipelines/bam_to_plastome_vcf.sh
+++ pipelines/bam_to_plastome_vcf.sh
@@ -13,7 +13,7 @@ do
 	if [ -f $f ]
 	then
 	echo "looking in $f"
-	# this cuts out the first ~2.2GB of the unsorted bam file: equivalent to >1000 reads/bp
+	# this cuts out the first ~2.2GB of the unsorted bam file: 10,000,000 (10M) reads is equivalent to >1000 reads/bp
 	python ~/phylogenomics/python/bwa_to_bam.py -i $f -r $REF -p 8 -n 10000000
 	python ~/phylogenomics/python/bam_to_vcf.py -i $f -r $REF -p 8
 	fi
