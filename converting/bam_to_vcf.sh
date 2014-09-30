4ca08ff18421db1506d489de12bda566451c1060
daisieh_local <daisieh@gmail.com>
2014-09-30 13:17:29 -0700

    -

--- converting/bam_to_vcf.sh
+++ converting/bam_to_vcf.sh
@@ -5,4 +5,4 @@ refname=$2
 
 echo "bam_to_vcf $1 $2"
 samtools faidx $refname
-samtools mpileup -B -E -C50 -f $refname -u $sample.sorted.bam | bcftools view -c > $sample.vcf
+samtools mpileup -B -E -C50 -f $refname -u $sample.sorted.bam | bcftools view -c - > $sample.vcf
