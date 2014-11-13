b2453d634ef4ef38ec9155e97cb5ae760cf57c03
daisieh_local <daisieh@gmail.com>
2014-11-12 22:13:05 -0800

    test

--- parsing/raxml_condense.pl
+++ parsing/raxml_condense.pl
@@ -60,7 +60,7 @@ if (!(-s "RAxML_info.$inputname")) {
 	close $fh;
 	# make sure that the phylip taxa names are accounted for:
 	$raxml_data->{"taxa"} = ();
-	my @x = split (/\t/, $phylip_str, @{$raxml_data->{"fulltaxa"}} + 1);
+	my @x = split (/\t/, $phylip_str);
 	shift @x;
 	print Dumper (@x);
 
