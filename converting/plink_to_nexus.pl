026eff7653fc6da8289607586a073334f7c8e7da
daisieh_local <daisieh@gmail.com>
2014-11-18 18:42:27 -0800

    test

--- converting/plink_to_nexus.pl
+++ converting/plink_to_nexus.pl
@@ -103,6 +103,7 @@ foreach my $line (<PED_FH>) {
 close PED_FH;
 print "mapping genotypes...\n";
 foreach my $indiv_id (@$indiv_array) {
+	print "looking at $indiv\n";
 	my $indiv = $individuals->{$indiv_id};
 	my $alleles = "$indiv->{alleles}";
 	$alleles =~ s/\s+//g;
