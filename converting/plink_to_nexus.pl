83096f0dcdeb879cc9cb0ceb7c9f27f7b1ffbb45
daisieh_local <daisieh@gmail.com>
2014-11-21 17:00:09 -0800

    move plink functions to module

--- converting/plink_to_nexus.pl
+++ converting/plink_to_nexus.pl
@@ -7,10 +7,10 @@ use Data::Dumper;
 use File::Temp qw(tempfile);
 use File::Basename qw(fileparse);
 use FindBin;
-use lib "$FindBin::Bin/..";
-use lib "$FindBin::Bin/../parsing";
+use lib "$FindBin::Bin/../lib";
 use Subfunctions qw(debug set_debug get_iupac_code consensus_str);
 use Nexus qw(write_nexus_character_block write_nexus_trees_block write_nexus_taxa_block);
+use Plink qw(parse_plink);
 
 my $help = 0;
 my $outfile = "";
@@ -66,97 +66,30 @@ if ($outfile !~ /\.nex$/) {
 	$outfile = "$outfile.nex";
 }
 
-print "processing .map file...\n";
-my $snps = ();
-open MAP_FH, "<", $inputmap;
-# map file:
-# col 1: chromosome (scaffold)
-# col 2: snp name
-# col 3: 0 (position in a genetic map)
-# col 4: position in the physical map (base pair)
-foreach my $line (<MAP_FH>) {
-	my $snp_hash = {};
-	if ($line =~ /^(.+?)\s+(.+?)\s+(.+?)\s+(.+?)$/) {
-		$snp_hash->{"chromosome"} = "$1";
-		$snp_hash->{"name"} = "$2";
-		$snp_hash->{"genetic_map_position"} = $3;
-		$snp_hash->{"base_pair"} = $4;
-	}
-	push @$snps, $snp_hash;
-}
-close MAP_FH;
-
-my $total_snp_count = @$snps;
-my $individuals = {};
-my $indiv_array = ();
-
-open PED_FH, "<", $inputped;
-# ped file:
-# 1 ALAA20-2_DNA55 0 0 2 2 C C
-# col 1: family ID
-# col 2: individual ID
-# col 3: paternal ID (0 for no information)
-# col 4: maternal ID (0 for no information)
-# col 5: sex (1 if male, 2 if female, 0 if unknown)
-# col 6: phenotype (a coded value for association genetics)
-# next cols are pairs of allelic values corresponding to the mapped snps, 0 for missing data
-print "processing .ped file...\n";
-foreach my $line (<PED_FH>) {
-	if ($line =~ /^(.+?)\s+(.+?)\s+(.+?)\s+(.+?)\s+(.+?)\s+(.+?)\s+(.*)$/) {
-		my $indiv_hash = {};
-		$indiv_hash->{"family_id"} = "$1";
-		$indiv_hash->{"individual_id"} = "$2";
-		$indiv_hash->{"paternal_id"} = "$3";
-		$indiv_hash->{"maternal_id"} = "$4";
-		$indiv_hash->{"sex"} = $5;
-		$indiv_hash->{"phenotype"} = $6;
-		$indiv_hash->{"alleles"} = "$7";
-		push @$indiv_array, $indiv_hash->{"individual_id"};
-		$individuals->{$indiv_hash->{"individual_id"}} = $indiv_hash;
-	}
-}
-close PED_FH;
-print "mapping genotypes...\n";
-foreach my $indiv_id (@$indiv_array) {
-	my $indiv = $individuals->{$indiv_id};
-	my $alleles = "$indiv->{alleles}";
-	$alleles =~ s/\s+//g;
-	$alleles =~ s/0/-/g; # replace any 0's with -'s.
-	print "looking at $indiv_id with " . (length $alleles)/2 . " alleles\n";
-	$indiv->{"paternal"} = ($alleles =~ s/([A-Za-z\-])[A-Za-z\-]/$1\./gr);
-	$indiv->{"paternal"} =~ s/\.//g;
-	$indiv->{"maternal"} = ($alleles =~ s/[A-Za-z\-]([A-Za-z\-])/\.$1/gr);
-	$indiv->{"maternal"} =~ s/\.//g;
-	my @seqarray = ($indiv->{"paternal"}, $indiv->{"maternal"});
-	my $genotype = consensus_str(\@seqarray);
-	$indiv->{"genotype"} = $genotype;
-	print $indiv->{"paternal"}."\n".$indiv->{"maternal"}."\n".$indiv->{"genotype"}."\n";
-	if ((length $genotype) != $total_snp_count) {
-		print "$indiv->{individual_id} has an incorrect number of snps specified in the ped file: ". (length $genotype) . " instead of $total_snp_count.\n";
-		exit;
-	}
-}
-
-#  print "individuals " .Dumper ($individuals);
+my $plink_hash = parse_plink($inputped, $inputmap);
 
 # write out as nexus:
 print "writing output to $outfile\n";
-my $nexusstring = "#NEXUS\n\n";
 
 my $nexushash = {};
-$nexushash->{"characters"} = {};
-$nexushash->{"taxa"} = $indiv_array;
-foreach my $indiv_id (@{$nexushash->{"taxa"}}) {
-	$nexushash->{"characters"}->{$indiv_id} = $individuals->{$indiv_id}->{"genotype"};
-}
 
+# set up taxa block:
+$nexushash->{"taxa"} = $plink_hash->{"names"};
+
+# set up characters block:
+$nexushash->{"characters"} = {};
+foreach my $indiv_id (@{$nexushash->{"taxa"}}) {
+	$nexushash->{"characters"}->{$indiv_id} = $plink_hash->{"individuals"}->{$indiv_id}->{"genotype"};
+}
+$nexushash->{"charlabels"} = $plink_hash->{"snp_names"};
+
+my $nexusstring = "#NEXUS\n\n";
 $nexusstring .= write_nexus_taxa_block($nexushash);
 $nexusstring .= write_nexus_character_block($nexushash);
 
 open OUT_FH, ">", $outfile;
 print OUT_FH $nexusstring;
 close OUT_FH;
-
 
 __END__
 
---
+++ lib/Plink.pm
@@ -0,0 +1,142 @@
+package Plink;
+use strict;
+use Data::Dumper;
+use Subfunctions qw(debug set_debug consensus_str);
+
+
+BEGIN {
+	require Exporter;
+	# set the version for version checking
+	our $VERSION     = 1.00;
+	# Inherit from Exporter to export functions and variables
+	our @ISA         = qw(Exporter);
+	# Functions and variables which are exported by default
+	our @EXPORT      = qw();
+	# Functions and variables which can be optionally exported
+	our @EXPORT_OK   = qw(parse_plink parse_ped parse_map);
+}
+
+=head1
+
+Plink data structure:
+These come from the ped file:
+	$plink_hash->{"individuals"}: a hash of hashes, keyed by individual_id
+		->{"family_id"}: family ID
+		->{"individual_id"}: individual ID
+		->{"paternal_id"}: paternal ID (0 for no information)
+		->{"maternal_id"}: maternal ID (0 for no information)
+		->{"sex"}: sex (1 if male, 2 if female, 0 if unknown)
+		->{"phenotype"}: phenotype (a coded value for association genetics)
+		->{"paternal"}: the paternal haplotype
+		->{"maternal"}: the maternal haplotype
+		->{"genotype"}: the consensus genotype
+	$plink_hash->{"names"}: an array of the names of the individuals
+These come from the map file:
+	$plink_hash->{"snps"}: a hash of hashes, keyed by snp name
+		->{"chromosome"}: chromosome (scaffold)
+		->{"name"}: snp name
+		->{"genetic_map_position"}: 0 (position in a genetic map)
+		->{"base_pair"}: position in the physical map (base pair)
+	$plink_hash->{"snp_names"}: an array of the names of the snps
+=cut
+
+sub parse_plink {
+	my $inputped = shift;
+	my $inputmap = shift;
+	my $plink_hash = shift;
+
+	unless (defined $plink_hash) {
+		$plink_hash = {};
+	}
+	parse_ped ($inputped, $plink_hash);
+	parse_map ($inputmap, $plink_hash);
+	return $plink_hash;
+}
+
+sub parse_ped {
+	my $inputped = shift;
+	my $plink_hash = shift;
+
+	unless (defined $plink_hash) {
+		$plink_hash = {};
+	}
+
+	$plink_hash->{"individuals"} = {};
+	$plink_hash->{"names"} = ();
+
+	open PED_FH, "<", $inputped;
+	# ped file:
+	# 1 ALAA20-2_DNA55 0 0 2 2 C C
+	# col 1: family ID
+	# col 2: individual ID
+	# col 3: paternal ID (0 for no information)
+	# col 4: maternal ID (0 for no information)
+	# col 5: sex (1 if male, 2 if female, 0 if unknown)
+	# col 6: phenotype (a coded value for association genetics)
+	# next cols are pairs of allelic values corresponding to the mapped snps, 0 for missing data
+	foreach my $line (<PED_FH>) {
+		if ($line =~ /^(.+?)\s+(.+?)\s+(.+?)\s+(.+?)\s+(.+?)\s+(.+?)\s+(.*)$/) {
+			my $indiv_hash = {};
+			$indiv_hash->{"family_id"} = "$1";
+			$indiv_hash->{"individual_id"} = "$2";
+			$indiv_hash->{"paternal_id"} = "$3";
+			$indiv_hash->{"maternal_id"} = "$4";
+			$indiv_hash->{"sex"} = $5;
+			$indiv_hash->{"phenotype"} = $6;
+			$indiv_hash->{"alleles"} = "$7";
+			push @{$plink_hash->{"names"}}, $indiv_hash->{"individual_id"};
+			$plink_hash->{"individuals"}->{$indiv_hash->{"individual_id"}} = $indiv_hash;
+		}
+	}
+	close PED_FH;
+
+	foreach my $indiv_id (@{$plink_hash->{"names"}}) {
+		my $indiv = $plink_hash->{"individuals"}->{$indiv_id};
+		my $alleles = delete $indiv->{alleles};
+		$alleles =~ s/\s+//g;
+		$alleles =~ s/0/-/g; # replace any 0's with -'s.
+		$indiv->{"paternal"} = ($alleles =~ s/([A-Za-z\-])[A-Za-z\-]/$1\./gr);
+		$indiv->{"paternal"} =~ s/\.//g;
+		$indiv->{"maternal"} = ($alleles =~ s/[A-Za-z\-]([A-Za-z\-])/\.$1/gr);
+		$indiv->{"maternal"} =~ s/\.//g;
+		my @seqarray = ($indiv->{"paternal"}, $indiv->{"maternal"});
+		my $genotype = consensus_str(\@seqarray);
+		$indiv->{"genotype"} = $genotype;
+	}
+
+	return $plink_hash;
+}
+
+sub parse_map {
+	my $inputmap = shift;
+	my $plink_hash = shift;
+
+	unless (defined $plink_hash) {
+		$plink_hash = {};
+	}
+
+	$plink_hash->{"snps"} = {};
+	open MAP_FH, "<", $inputmap;
+	# map file:
+	# col 1: chromosome (scaffold)
+	# col 2: snp name
+	# col 3: 0 (position in a genetic map)
+	# col 4: position in the physical map (base pair)
+	foreach my $line (<MAP_FH>) {
+		my $snp_hash = {};
+		if ($line =~ /^(.+?)\s+(.+?)\s+(.+?)\s+(.+?)$/) {
+			$snp_hash->{"chromosome"} = "$1";
+			$snp_hash->{"name"} = "$2";
+			$snp_hash->{"genetic_map_position"} = $3;
+			$snp_hash->{"base_pair"} = $4;
+		}
+		$plink_hash->{"snps"}->{$snp_hash->{"name"}} = $snp_hash;
+		push @{$plink_hash->{"snp_names"}}, $snp_hash->{"name"};
+	}
+	close MAP_FH;
+	return $plink_hash;
+}
+
+
+# must return 1 for the file overall.
+1;
